f08d9bfabb181d8aa1db7f5f6ff1316300037d38
Yhojan Caro“ <caroyhojan@gmail.com>
2026-02-09 19:08:07 -0500

    Primer commit“

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+  const fecha = new Date().toLocaleString();
+
+  return (
+    <main>
+      <h1>Hola Soy Yhojan Caro - "Aprendiz ADSO"</h1>
+      <h2>Quiero aprender todo lo relacionado con el desarrollo web y de aplicaciones</h2>
+    </main>
+  )
+}
